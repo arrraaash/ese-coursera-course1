f67be2fe367884fad98e205f8274604b2eacefce
arrraaash <arash.amini.71@gmail.com>
2023-10-28 13:06:33 +0200

    third commit

--- stats.h
+++ stats.h
@@ -8,14 +8,14 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-**
+/**
  * @file stats.h 
  * @brief declaration of functions
  *
  * declaring functions which are working on an input unsigned char array
  *
  * @author <Arash Amini>
- * @date 28-10-2023
+ * @date 25-10-2023
  *
  */
 #ifndef __STATS_H__
@@ -24,17 +24,100 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief sort the array of unsiged char
  *
- * <Add Extended Description Here>
+ * This function takes an unsigned char array and the size if that array as an 
+ * input and returns the sorted version of that array from largest element to
+ * smallest. 
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param unsignedchar The array that will be sorted
+ * @param int size of the input array
  *
- * @return <Add Return Informaiton here>
+ * @return The pointer to the sorted array
  */
+unsigned char * sort_array( unsigned char *a, int size);
+
+/**
+ * @brief find median of an array
+ *
+ * This function takes an unsigned char array and its size first sort it by useing the sort function, 
+ * then take the middle value if the array size was odd, in case if even, it will give the mean of two
+ * middle values. 
+ *
+ * @param unsignedchar The array that will be sorted
+ * @param int size of the input array
+ *
+ *
+ * @return an double value
+ */
+double find_median(unsigned char *a, int size);
+
+/**
+ * @brief compute the mean of and array of unsigned char values
+ *
+ * This function takes an unsigned char array and the size add all elements together and devide them
+ * by the size. 
+ *
+ * @param unsignedchar The array that will be sorted
+ * @param int size of the input array
+ *
+ * @return returns a double value as mean
+ */
+double find_mean(unsigned char *a, int size);
+
+/**
+ * @brief compute the minimum
+ *
+ * This function takes an unsigned char array and the size, first it will sort in by useing
+ * sort function, then take the last element due to type of sort which is from larg to small. 
+ *
+ * @param unsignedchar The array that will be sorted
+ * @param int size of the input array
+ *
+ * @return returns an minimum value of array as unsigned char value
+ */
+unsigned char find_minimum(unsigned char *a, int size);
+
+/**
+ * @brief compute the maximum
+ *
+ * This function takes an unsigned char array and the size, first it will sort in by useing
+ * sort function, then take the first element due to type of sort which is from larg to small. 
+ *
+ * @param unsigned char The array that will be sorted
+ * @param int size of the input array
+ *
+ * @return returns an maximum value of array as unsigned char value
+ */
+unsigned char find_maximum(unsigned char *a, int size);
+
+/**
+ * @brief print the statistical features of an array
+ *
+ * This function takes an unsigned char array and the size, compute minimum, maximum, mean,
+ * and median of that array and print all this values.
+ *
+ * @param unsigned char The array that will be sorted
+ * @param int size of the input array
+ *
+ *
+ * @return nothing
+ */
+void print_statistics(unsigned char *a, int size);
+
+/**
+ * @brief print the inout array
+ *
+ * This function takes an unsigned char array and the size and print it on the screen.
+ *
+ * @param unsigned char The array that will be sorted
+ * @param int size of the input array
+ *
+ * @return nothing
+ */
+void print_array( unsigned char *a, int size);
+
+
 
 
 #endif /* __STATS_H__ */
